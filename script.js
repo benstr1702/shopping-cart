174385c9a6a1d58d4fe1bc1ba5f8fc233ac5329a
ben strugo <66129129+benstr1702@users.noreply.github.com>
2024-07-14 14:24:57 +0300

    added active highlighting for icons

--- script.js
+++ script.js
@@ -1,81 +1,50 @@
-// const iconSection = document.querySelector("#icon-stock-section");
-// const cartSection = document.querySelector("#cart-stock-section");
-// const userIconDiv = document.querySelector(".user-icon");
-// const userCartDiv = document.querySelector(".user-cart");
+const avatarStockElement = document.getElementById("icon-stock-section");
 
-// avatarIconNames.forEach((icon) => {
-//   let iconElement = document.createElement("i");
-//   iconElement.className = icon;
-//   iconElement.style.color = `rgb(${chooseRandomColor()} , ${chooseRandomColor()} , ${chooseRandomColor()})`;
-//   iconElement.style.fontSize = "40px";
-//   iconSection.appendChild(iconElement);
-
-//   iconElement.addEventListener("click", () => {
-//     // Remove active class from all icons in the original section
-//     document.querySelectorAll("#icon-stock-section i").forEach((icon) => {
-//       icon.classList.remove("active");
-//     });
-
-//     // Add active class to the clicked icon in the original section
-//     iconElement.classList.add("active");
-
-//     // Clear previous content of userIconDiv
-//     userIconDiv.innerHTML = "";
-
-//     // Create a deep clone of the clicked icon
-//     let clonedIcon = iconElement.cloneNode(true);
-//     console.log(clonedIcon);
-
-//     userIconDiv.appendChild(clonedIcon);
-
-//     // Log to console for debugging
-//     console.log("Icon cloned and appended to user icon div");
-//   });
-// });
-
-// //cart
-// cartIconNames.forEach((iconClass) => {
-//   let element = document.createElement("i");
-//   element.className = iconClass;
-//   element.style.color = `rgb(${chooseRandomColor()} , ${chooseRandomColor()} , ${chooseRandomColor()})`;
-//   element.style.fontSize = "40px";
-//   cartSection.appendChild(element);
-
-//   element.addEventListener("click", () => {
-//     // Remove active class from all icons in the cart section
-//     document.querySelectorAll("#cart-stock-section i").forEach((icon) => {
-//       icon.classList.remove("active");
-//     });
-
-//     // Add active class to the clicked icon
-//     element.classList.add("active");
-//     userCartDiv.innerHTML = "";
-
-//     // Create a deep clone of the clicked icon
-//     let clonedCart = element.cloneNode(true);
-//     console.log(clonedCart);
-
-//     userCartDiv.appendChild(clonedCart);
-//   });
-// });
-
+const cartStockElement = document.getElementById("cart-stock-section");
 //Choose random color function
 function chooseRandomColor() {
   let random = Math.floor(Math.random() * 256);
   return random;
 }
+//Avatar Class
+class Avatar {
+  constructor(htmlElement, iconClasses) {
+    this.htmlElement = htmlElement;
+    this.htmlElement.className = iconClasses;
+    console.log(this.htmlElement, " htmlElement");
+    this.htmlElement.addEventListener("click", this.toggleActive.bind(this));
+  }
+  toggleActive() {
+    console.log("toggled");
+    this.htmlElement.classList.toggle("active");
+  }
+}
 //Stock Class
 class Stock {
   constructor(htmlElement, iconClassList) {
-    this.items = [];
+    // this.items = [];
     this.htmlElement = htmlElement;
     this.iconClassList = iconClassList;
+    this.createStock();
   }
 
   createStock() {
-    this.stock = this.iconClassList.map((iconClasses) => {
+    const shuffledIconClassList = [...this.iconClassList];
+    for (let i = shuffledIconClassList.length - 1; i > 0; i--) {
+      // Fixed: shuffledIconClassList - 1 -> shuffledIconClassList.length - 1
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledIconClassList[i], shuffledIconClassList[j]] = [
+        shuffledIconClassList[j],
+        shuffledIconClassList[i],
+      ];
+    }
+    console.log("Shuffled icon classes:", shuffledIconClassList);
+
+    this.stock = shuffledIconClassList.map((iconClasses) => {
       const newIcon = document.createElement("i");
+      newIcon.style.color = `rgb(${chooseRandomColor()} , ${chooseRandomColor()} , ${chooseRandomColor()})`;
       this.htmlElement.appendChild(newIcon);
+      // console.log(new Avatar(newIcon, iconClasses));
       return new Avatar(newIcon, iconClasses);
     });
   }
@@ -83,11 +52,5 @@ class Stock {
 
 //Avatar object from Stock
 const avatarStock = new Stock(avatarStockElement, avatarIconNames);
-
-//Avatar Class
-class Avatar {
-  constructor(htmlElement, iconClasses) {
-    this.htmlElement = htmlElement;
-    this.htmlElement.className = iconClasses;
-  }
-}
+// cart stock from Stock
+const cartStock = new Stock(cartStockElement, cartIconNames);
